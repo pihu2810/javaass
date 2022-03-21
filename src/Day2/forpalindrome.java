f7490084079d8011a2d523730347318928dd4955
pihu2810 <vishaka.kadam01@gmail.com>
2022-03-22 00:06:24 +0530

    add forloop3

---
+++ src/Day2/forpalindrome.java
@@ -0,0 +1,21 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class forpalindrome {
+	 public static void main(String args[])  
+	   {  
+	      String original, reverse = ""; 
+	      Scanner in = new Scanner(System.in);   
+	      System.out.println("Enter a number ");  
+	      original = in.nextLine(); 
+	      in.close();
+	      int length = original.length();   
+	      for ( int i = length - 1; i >= 0; i-- )  
+	         reverse = reverse + original.charAt(i);  
+	      if (original.equals(reverse))  
+	         System.out.println("Entered number is a palindrome.");  
+	      else  
+	         System.out.println("Entered number is not a palindrome.");   
+	   }  
+	}  
