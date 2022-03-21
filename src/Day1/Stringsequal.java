5c97c547f7d84c1bfc4a667ece1cf678d7f163c5
pihu2810 <vishaka.kadam01@gmail.com>
2022-03-21 16:14:04 +0530

    add string

---
+++ src/Day1/Stringsequal.java
@@ -0,0 +1,20 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Stringsequal {
+	public static void main(String argd[])
+	{
+		 String str1, str2;
+		Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter first String:");
+		str1=sc.nextLine();
+		 System.out.println("Enter first String:");
+		 str2=sc.nextLine();
+		 if(str1.equals(str2))
+			 System.out.println("Strings are Equal");	
+		 else
+		     System.out.println("Strings are Not Equal");
+	}
+
+}
