c86ccaa6bf2b127e19936ff0fea1309f3e548b51
pihu2810 <vishaka.kadam01@gmail.com>
2022-03-22 16:21:28 +0530

    add Array1

---
+++ src/Day3/Elementofarray.java
@@ -0,0 +1,12 @@
+package Day3;
+
+public class Elementofarray {
+ public static void main(String args[])
+ {
+	 int[] arr= { -2,-3,4,7,8,23,35};
+	 System.out.print("Elements of given array are: ");
+	 for(int i=0;i<arr.length;i++) {
+		 System.out.print(arr[i] + " ");
+	 }
+ } 
+}
